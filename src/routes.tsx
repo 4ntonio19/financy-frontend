a03d88de832ca7a309ed90d63755c9b5e96e6614
4ntonio19 <vgoncalvescarvalho@outlook.com>
2024-03-19 23:40:59 -0300

    test: deploy vercel

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import WelcomePage from "./pages/welcomePage";
-import Home from "./pages/home";
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/home" element={<Home />} />
+        {/* <Route path="/home" element={} /> */}
       </Routes>
     </Router>
   );

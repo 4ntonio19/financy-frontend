2c66e55c58bb6da206b91ec5278778a5754f22e4
4ntonio19 <vgoncalvescarvalho@outlook.com>
2024-03-19 23:53:31 -0300

    teste: deploy vercel 4

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import HomeContent from "../components/home-content";
+import PageModel from "../components/page-model";
+
+const Home = () => {
+  return (
+    <PageModel>
+      <HomeContent />
+    </PageModel>
+  );
+};
+
+export default Home;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import WelcomePage from "./pages/welcomePage";
-import HomeMain from "./pages/home/index.tsx";
+import WelcomePage from "./pages/welcomePage/index.tsx";
+import Home from "./pages/index.tsx";
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/home" element={<HomeMain />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
